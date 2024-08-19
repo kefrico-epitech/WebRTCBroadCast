292889d0a7223beaa90a0752369a8896c3ed9f8f
kefrico-epitech <kevin.coco@epitech.eu>
2024-08-19 17:06:12 +0100

    fix server

--- public/config.js
+++ public/config.js
@@ -1,7 +1,23 @@
 const Config = {
     host: "https://webrtctest-2rt6.onrender.com",
     iceServers: [
-        { urls: "stun:stun.l.google.com:19302" },
-        { urls: "stun:global.stun.twilio.com:3478" }
+        {
+            urls: "stun:global.stun.twilio.com:3478"
+        },
+        {
+            urls: "turn:global.turn.twilio.com:3478?transport=udp",
+            username: "14515d817094261dda4d38e5ff4b6dada910ea5c9b40d37d0f59b9f924014374",
+            credential: "quwWLRZva7hjMPjitdZJ/1w9OtMtKMXyP2XNDii9Ioo="
+        },
+        {
+            urls: "turn:global.turn.twilio.com:3478?transport=tcp",
+            username: "14515d817094261dda4d38e5ff4b6dada910ea5c9b40d37d0f59b9f924014374",
+            credential: "quwWLRZva7hjMPjitdZJ/1w9OtMtKMXyP2XNDii9Ioo="
+        },
+        {
+            urls: "turn:global.turn.twilio.com:443?transport=tcp",
+            username: "14515d817094261dda4d38e5ff4b6dada910ea5c9b40d37d0f59b9f924014374",
+            credential: "quwWLRZva7hjMPjitdZJ/1w9OtMtKMXyP2XNDii9Ioo="
+        }
     ]
 };
